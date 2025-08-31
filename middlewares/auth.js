5d1257c10ad2535579218579f184b83937662158
AbhijitSarkaar <abhijit.sarkaar26@gmail.com>
2025-08-31 18:40:30 +0530

    auth routes, image upload

---
+++ middlewares/auth.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+exports.protect = async (req, res, next) => {
+  const token = req.headers.authorization?.split(' ')[1];
+  if (!token) {
+    return res.status(401).json({
+      message: 'Not authorized. Missing token',
+    });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log('decoded', decoded);
+    req.user = await User.findById(decoded.userId).select('-password');
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      message: 'Not authorized. Invalid token',
+    });
+  }
+};
